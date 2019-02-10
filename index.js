8a5b232a3407f82d0a93ced2c9234d674b7b93cc
Fer Hernandez <fh.1111@gmail.com>
2019-02-10 20:44:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ function bake(updateFunction) {
 
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
-  setTimeout(function() {
-    bake.call(this,updateFunction)
+  setTimeout(() => {
+    bake.call(this, updateFunction)
   }, 2000)
   updateFunction(status)
 }
